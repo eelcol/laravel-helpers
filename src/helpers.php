27a0f4be99be68c214bed4aeae2eb5bdd66adb43
Eelco Luurtsema <eelcoluurtsema@gmail.com>
2019-02-16 18:43:11 +0100

    Add safe_array as helper

--- src/helpers.php
+++ src/helpers.php
@@ -25,3 +25,33 @@ if(!function_exists('display_messages'))
 		return Messages::view();
 	}
 }
+
+if(!function_exists('safe_array'))
+{
+	/**
+	* Retrieve a key from an array when not known if the array is set
+	*/
+	function safe_array($array, ...$params)
+	{
+		if(!is_array($array))
+		{
+			return NULL;
+		}
+
+		$firstParam = array_shift($params);
+		if(!isset($array[$firstParam]))
+		{
+			return NULL;
+		}
+
+		if(count($params) > 0)
+		{
+			/**
+			* Value not found yet
+			*/
+			return call_user_func_array("safe_array", array_merge([$array[$firstParam]], $params));
+		}
+
+		return $array[$firstParam];
+	}
+}
